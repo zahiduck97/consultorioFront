ca3355f4b15227f5d0589ad97c0c015544ee2006
AJUAREZP <aracely.juarez@gnp.com.mx>
2023-08-22 10:21:42 -0600

    Agregando PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import { LoginComponent } from './common/components/login/login.component';
 import { NotFoundComponent } from './common/components/not-found/not-found.component';
 import { HeaderComponent } from './common/components/header/header.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,13 @@ import {MatToolbarModule} from "@angular/material/toolbar";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
